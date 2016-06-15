21a55b0e2cfb24180a8f79f9410dcaf57d7a6113
Eugenia <yarutoua@gmail.com>
2016-06-15 14:19:29 +0200

    Commented typed_rewriting due to syntax errors

--- regraph/library/rewriters.py
+++ regraph/library/rewriters.py
@@ -21,7 +21,6 @@ from regraph.library.primitives import (merge_nodes,
                                         update_node_attrs)
 from regraph.library.utils import is_subdict
 from regraph.library.data_structures import (Homomorphism)
-from regraph.library.category_op import (final_PBC, pushout)
 
 
 class Rewriter:
@@ -216,11 +215,11 @@ class Rewriter:
                     else:
                         fold_dict["__NOT_DEFINED__"] = [n]
         for (k, l) in fold_dict.items():
-            merge_nodes(self.graph_, l, node_name=attr + ":" + k)
+            merge_nodes(self.graph_, l, node_name=attr+":"+k)
             for n in l:
                 for n0 in self.fully_expanded_graph.nodes():
                     if self.h_exp.mapping_[n0] == n:
-                        self.h_exp.mapping_[n0] = attr + ":" + k
+                        self.h_exp.mapping_[n0] = attr+":"+k
 
     def fold_nodes(self, l):
         ty = []
@@ -232,37 +231,35 @@ class Rewriter:
                 raise ValueError(
                     "Node %s isn't a valid node" % str(n)
                 )
-        new_ty = fold_left(lambda x, acc: acc + "_" + x, "_", sorted(ty))
+        new_ty = fold_left(lambda x, acc : acc+"_"+x, "_", sorted(ty))
         i = 0
-        new_name = new_ty + "0"
+        new_name = new_ty+"0"
         while(new_name in self.graph_.nodes()):
             i += 1
-            new_name = new_ty + str(i)
+            new_name = new_ty+str(i)
         for n in l:
             cast_node(self.graph_, n, new_ty)
             for n0 in self.fully_expanded_graph.nodes():
                 if self.h_exp.mapping_[n0] == n:
                     self.h_exp.mapping_[n0] = new_name
-        merge_nodes(self.graph_, l, node_name=new_name)
+        merge_nodes(self.graph_, l, node_name = new_name)
+
 
     def expand_node(self, n0):
         origin = []
         for n in self.fully_expanded_graph.nodes():
             if self.h_exp.mapping_[n] == n0:
                 origin.append(n)
-        if len(origin) > 1:
+        if len(origin) > 1 :
             exp_graph = self.fully_expanded_graph.subgraph(origin).copy()
             self.graph_ = union(self.graph_, exp_graph)
             for n in exp_graph.nodes():
                 neighbors = self.fully_expanded_graph.neighbors(n)
                 for n1 in neighbors:
-                    self.graph_.add_edge(
-                        n,
-                        self.h_exp.mapping_[n1],
-                        self.fully_expanded_graph.get_edge(n, n1))
+                    self.graph_.add_edge(n, self.h_exp.mapping_[n1], self.fully_expanded_graph.get_edge(n,n1))
                 self.h_exp.mapping_[n] = n
             remove_node(self.graph_, n0)
-        elif len(origin) == 1:
+        elif len(origin) == 1 :
             warnings.warn(
                 "Node %s is atomic, can't expand it further" % str(origin[0]),
                 Warning
@@ -360,8 +357,7 @@ class Rewriter:
             raise ValueError("Preserving part does not match!")
 
         RHS_instance =\
-            dict([(r, instance[left_h.mapping_[p]])
-                  for p, r in right_h.mapping_.items()])
+            dict([(r, instance[left_h.mapping_[p]]) for p, r in right_h.mapping_.items()])
         P_instance =\
             dict([(p, instance[l]) for p, l in left_h.mapping_.items()])
 
@@ -475,23 +471,23 @@ class Rewriter:
                 pass
         return RHS_instance
 
-    def typed_rewriting(self, left_h, right_h):
-        """ left_h : P -> L
-            right_h : P -> R
-        """
-        if left_h.source_ != right_h.source_:
-            raise ValueError(
-                "Can't rewrite, homomorphisms don't have the same preserved part"
-            )
+    # def typed_rewriting(self, left_h, right_h):
+    #     """ left_h : P -> L
+    #         right_h : P -> R
+    #     """
+    #     if left_h.source_ != right_h.source_:
+    #         raise ValueError(
+    #             "Can't rewrite, homomorphisms don't have the same preserved part"
+    #         )
 
-        L_G_instances = self.find_matching(left_h.target_)
-        Gprime = self.graph_
-        for L_G in L_G_instances :
-            G-, P_G-, G-_G = pullback_complement(left_h, L_G)
-            Gprime, G-_Gprime, R_Gprime = pushout(P_G-, P_R)
-            Gprime.metamodel_ = self.graph_.metamodel_
-            Gprime.hom = Homomorphism.canonic_homomorphism (Gprime, self.metamodel_)
-        return G-_Gprime, G-_G
+    #     L_G_instances = self.find_matching(left_h.target_)
+    #     Gprime = self.graph_
+    #     for L_G in L_G_instances :
+    #         G-, P_G-, G-_G = pullback_complement(left_h, L_G)
+    #         Gprime, G-_Gprime, R_Gprime = pushout(P_G-, P_R)
+    #         Gprime.metamodel_ = self.graph_.metamodel_
+    #         Gprime.hom = Homomorphism.canonic_homomorphism (Gprime, self.metamodel_)
+    #     return G-_Gprime, G-_G
 
     def generate_rule(self, LHS, commands):
         """Cast sequence of commands to homomorphisms."""
